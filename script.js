f7df7691788afa58c95bbfa7284f1fa5a9a31d39
Soren568 <sorheitmann@gmail.com>
2021-08-20 10:06:24 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// Sam
+var sam_likes = 0;
+var countSam = document.querySelector(".likes-Sam");
+
+function like_Sam(){
+    sam_likes++;
+    countSam.innerText = sam_likes + " like(s)"
+}
+
+// Sigourney
+var sig_likes = 0;
+var countSig = document.querySelector(".likes-Sig");
+
+function like_Sig(){
+    sig_likes++;
+    countSig.innerText = sig_likes + " like(s)"
+}
+
+// Zoe
+var zoe_likes = 0;
+var countZoe = document.querySelector(".likes-Zoe");
+
+function like_Zoe(){
+    zoe_likes++;
+    countZoe.innerText = zoe_likes + " like(s)"
+}
+
